cf3eb332a8643046b2b47281a393349441349436
Matthew Nieva <matthewcn56@gmail.com>
2021-08-06 16:31:28 -0700

    Initial server setup

---
+++ route_config.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def hello():
+    return "Our backend server!"
---
+++ server_config.py
@@ -0,0 +1,7 @@
+import os
+from route_config import *
+
+# Run debug mode while testing
+app.debug = True
+
+app.run()

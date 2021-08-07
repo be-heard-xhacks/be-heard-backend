fd0c1187c7deee6051b031e47a8cd3bcc0384a95
Matthew Nieva <65370631+matthewcn56@users.noreply.github.com>
2021-08-06 22:02:00 -0700

    Auth flow (#3)
    * Tentative auth flow
    
    * Added auth
    
    * wtf gitignore
    
    * .env bruh

---
+++ auth_routes.py
@@ -0,0 +1,76 @@
+from datetime import date
+import os
+import datetime
+from posix import environ
+from route_config import *
+from flask import Flask, jsonify, make_response, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+from bson.objectid import ObjectId
+import jwt
+import datetime
+# function to wrap around our db calls around
+
+def auth_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        auth_token = None
+        if 'access-token' in request.headers:
+            auth_token = request.headers['access-token']
+        
+        if not auth_token:
+            return jsonify({'message' : 'no auth token'})
+        try:
+            jwt_data = jwt.decode(auth_token, os.environ.get("PASSWORD_SALT"), algorithms=["HS256"])
+            current_user = str(db.users.find_one_or_404({"_id": ObjectId(jwt_data['_id']) }))
+        except:
+            return jsonify({"message": 'token is invalid'})
+        return f(current_user, *args, **kwargs)
+    return decorator
+
+# print("Password salt is: " +os.environ.get("PASSWORD_SALT") )
+
+
+@app.route('/register', methods = ['POST'])
+def registerUser():
+    user_pass=request.args.get("password")
+    user_email = request.args.get("email")
+
+    prev_user = db.users.find_one( {"email": user_email})
+    if prev_user:
+        return jsonify({"message": "Error, email is taken"})
+
+    hashed_password = generate_password_hash(user_pass, method = 'sha256')
+    new_user = db.users.insert_one({
+        "email": user_email,
+        "password" : hashed_password
+    })
+    return jsonify({"message" : "User successfully registered" })
+
+@app.route('/login', methods = ['POST'])
+def login_user():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return make_response("Must login", 401, {'Authentication': 'login required'})
+    print("auth.username is: " + auth.username)
+    user = db.users.find_one_or_404({ "email" : auth.username})
+    print(user)
+    if check_password_hash(user["password"],auth.password):
+        jwt_token = jwt.encode({ 
+            '_id': str(user['_id']), 
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=4)
+            },
+            os.environ.get("PASSWORD_SALT"), "HS256")
+        return jsonify({'jwt_token': jwt_token})
+    return make_response('Invalid username or password',  401, {'Authentication': '"login required"'})
+    
+
+
+@app.route("/getGag", methods = ['GET'])
+def hello_bobert():
+    #gag = db.users.find_one({"email": "gag"})
+    #print ("Gag is: " + db.users.find_one({"email": "gag"})["_id"])
+    return jsonify({"message": str(db.users.find_one({"email": "gag"})["_id"])}) 
+
+
+
--- route_config.py
+++ route_config.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
-import os
-
 from dotenv import load_dotenv
-load_dotenv()
+import os
+# REMOVE BEFORE PUSHING!!!
+# load_dotenv()
 
 app = Flask(__name__)
-app.config['MONGO_URI']=os.environ['URI']
+app.config["MONGO_URI"] = os.environ['URI']
 mongo = PyMongo(app)
 db = mongo.db
-
 @app.route("/", methods = ['GET'])
 def hello():
     return "Our backend server!"

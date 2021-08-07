b8ae5bf6848a742807455e6d8c515c96ad8a6a7c
Matthew Nieva <matthewcn56@gmail.com>
2021-08-06 18:09:27 -0700

    Tested port

--- server_config.py
+++ server_config.py
@@ -4,4 +4,6 @@ from route_config import *
 # Run debug mode while testing
 # app.debug = True
 
-app.run()
+host = os.environ.get("IP", '0.0.0.0')
+port = int(os.environ.get("PORT", 8080))
+app.run(host = host, port = port)
